1f1c2495975550567c9fb1dced952d83d3f844fb
Pav <pavel-m@live.com>
2018-05-19 00:58:48 -0400

    Finished
    Completed the React Tutorial from the Solidity Course

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 // Connect React from the installed modules
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyCJaJBJKaN2-gzUF_vTBY1LsGNAPmdwkKw';
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it
